f185a63b02da7d8f08bd4541a0f1d24b8cfe47e9
iurymateus <iury.mateusg3@gmail.com>
2026-07-16 15:33:04 -0300

    Add files via upload

---
+++ sw.js
@@ -0,0 +1,18 @@
+const CACHE = 'jarvis-v1';
+const URLS = ['index.html', 'manifest.json'];
+
+self.addEventListener('install', (e) => {
+  e.waitUntil(
+    caches.open(CACHE).then(c => c.addAll(URLS)).then(() => self.skipWaiting())
+  );
+});
+
+self.addEventListener('activate', (e) => {
+  e.waitUntil(clients.claim());
+});
+
+self.addEventListener('fetch', (e) => {
+  e.respondWith(
+    caches.match(e.request).then(r => r || fetch(e.request).catch(() => caches.match('index.html')))
+  );
+});
